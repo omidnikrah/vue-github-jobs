b85bdee7f665fa2a7eabbab4fe353f5424ed8c7d
o.nikrah <o.nikrah@sabaidea.com>
2019-05-12 15:47:28 +0430

    init commit 🎉

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header';
+  export default {
+    name: 'app',
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Baloo+Bhai|Roboto');
+  * {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    font-family: 'Roboto', cursive;
+    outline: 0;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header id="header">
+    <h1 id="title">Github Jobs</h1>
+    <form id="search-form">
+      <input name="title" type="text" placeholder="Job title..." />
+      <input name="title" type="text" placeholder="Location..." />
+    </form>
+  </header>
+</template>
+
+<style scoped>
+#header {
+  background-color: #f9f9f9;
+  text-align: center;
+  padding: 50px;
+}
+#title {
+  text-align: center;
+  font-size: 50px;
+  margin: 0;
+  color: #333;
+  font-family: 'Baloo Bhai', cursive;
+}
+#search-form input {
+  padding: 15px;
+  -webkit-appearance: none;
+  border: 0;
+  margin: 5px;
+  min-width: 15%;
+  border-radius: 5px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.05);
+  font-size: 13px;
+}
+</style>
